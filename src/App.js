2eec9f9e61558c1b25d498ad5b6d15951d833892
Emilia Starczyk <emiliastarczyk@gmail.com>
2022-05-28 15:05:46 +0200

    ajv 2019 for dependentRequired, error display

--- src/App.js
+++ src/App.js
@@ -7,21 +7,35 @@ function App() {
   const [data, setData] = useState(loadData);
   const [file, setFile] = useState(null);
   const [disabledButton, setDisabledButton] = useState(true);
-  console.log(data);
+  const [error, setError] = useState('');
+
   function onChange(e) {
-    const reader = new FileReader();
+    e.preventDefault();
+    let reader = new FileReader();
     reader.onload = function (evt) {
-      const file = evt.target.result;
-      const parsedFile = JSON.parse(file);
-      const result = checkUploadedFile(parsedFile);
-      if (result) {
-        setFile(parsedFile);
-        setDisabledButton(false);
+      let file = evt.target.result;
+      try {
+        let parsedFile = JSON.parse(file);
+        let result = checkUploadedFile(parsedFile);
+        if (result) {
+          setFile(parsedFile);
+          setDisabledButton(false);
+          setError('');
+        }
+        else
+          setError('Your file does not match schema');
       }
-      else
-        console.log('problems');
+      catch (e) {
+        console.log(e);
+        setError('Parse problems')
+      }
+
     };
     reader.readAsText(e.target.files[0]);
+  }
+
+  function onClick(e) {
+    e.target.value = null;
   }
 
 
@@ -42,8 +56,9 @@ function App() {
         <label htmlFor='fileInput' className='buttonLabel'>
           Choose JSON file with list
         </label>
+        <p className='error'>{error}</p>
         <input type='file' accept='.json' id='fileInput' multiple={false}
-          onChange={onChange} style={{ display: 'none' }} />
+          onChange={onChange} style={{ display: 'none' }} onClick={onClick} />
         <div className='expandableListBox'>
           <h2>List</h2>
           <button type="button" onClick={generateList} disabled={disabledButton} className="generateButton">
--- src/Data.js
+++ src/Data.js
@@ -2,12 +2,16 @@ export function loadData() {
     let json = require('./TestFiles/doc.json');
     return json;
 }
-let schema = require('./TestFiles/schema.json');
 
 export function checkUploadedFile(data) {
-    const Ajv = require("ajv");
-    const ajv = new Ajv();
-    const valid = ajv.validate(schema, data);
+    let schema = require('./TestFiles/schema.json');
+    let Ajv2019 = require("ajv/dist/2019");
+
+    let ajv = new Ajv2019();
+    ajv.addMetaSchema(require('ajv/lib/refs/json-schema-draft-06.json'));
+    let validate = ajv.compile(schema);
+
+    let valid = validate(data);
     if (!valid)
         console.log(ajv.errors);
     else
